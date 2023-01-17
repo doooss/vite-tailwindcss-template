089cc31f26bb3ac9b773a8f3e47a63f3d7a2af1b
doyoung <ldy9212@gmail.com>
2023-01-17 17:56:49 +0900

    🎉 init: template init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import stylelint from 'vite-plugin-stylelint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        stylelint({
+            fix: true,
+            files: ['src/**/*.{css}'],
+        }),
+    ],
+});
